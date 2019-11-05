d989b2abae5215dae4f7c536ad8b4c60a28c1bbb
eliyaz-virtech <54634439+eliyaz-virtech@users.noreply.github.com>
2019-11-05 11:31:33 +0530

    Add files via upload

---
+++ tiffConversion.py
@@ -0,0 +1,49 @@
+
+'''
+from PIL import Image
+im = Image.open('test.tiff')
+im.save('test.jpeg')
+'''
+
+
+from PIL import Image
+import glob
+
+for name in glob.glob('*.tif'):
+    im = Image.open("a1.tif")
+    name = str("a1.tif").rstrip(".tif")
+    im.save("a1.jpg" + '.jpg', 'JPEG')
+
+print("Conversion from tif/tiff to jpg completed!")
+
+'''
+import os
+from PIL import Image
+
+yourpath = os.getcwd()
+for root, dirs, files in os.walk(yourpath, topdown=False):
+    for name in files:
+        print(os.path.join(root, name))
+        if os.path.splitext(os.path.join(root, name))[1].lower() == ".tiff":
+            if os.path.isfile(os.path.splitext(os.path.join(root, name))[0] + ".jpg"):
+                print "A jpeg file already exists for %s" % name
+            # If a jpeg is *NOT* present, create one from the tiff.
+            else:
+                outfile = os.path.splitext(os.path.join(root, name))[0] + ".jpg"
+                try:
+                    im = Image.open(os.path.join(root, name))
+                    print "Generating jpeg for %s" % name
+                    im.thumbnail(im.size)
+                    im.save(outfile, "JPEG", quality=100)
+                except Exception, e:
+                    print e
+        
+'''
+
+for name in glob.glob('*.tif'):
+    im = Image.open("a1.tif")
+    name = str("a1.tif").rstrip(".tif")
+    im.save("a1.png" + '.png', 'PNG')
+
+
+print("Conversion from tif/tiff to png completed!")
